e5403276efcceda1a51c23c7bee2f71a64f20f48
gaut-b <gautier.bayle@gmail.com>
2020-05-06 12:12:30 +0200

    correct static_url in settings.py

--- atelier_ideal/atelier_ideal/settings.py
+++ atelier_ideal/atelier_ideal/settings.py
@@ -152,7 +152,7 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
