444e2c2025668f9475d02dfec7bf19372bfbc80b
gaut-b <gautier.bayle@gmail.com>
2020-05-06 11:56:57 +0200

    bux fix

--- atelier_ideal/atelier_ideal/settings.py
+++ atelier_ideal/atelier_ideal/settings.py
@@ -155,7 +155,7 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
