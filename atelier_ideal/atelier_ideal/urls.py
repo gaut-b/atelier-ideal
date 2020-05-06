4d37d626fe25fc834d3e450b20d5596b9986fed8
gaut-b <gautier.bayle@gmail.com>
2020-05-06 12:26:50 +0200

    Undo changes in urls.py

--- atelier_ideal/atelier_ideal/urls.py
+++ atelier_ideal/atelier_ideal/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
     path('faicon/', include('faicon.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
