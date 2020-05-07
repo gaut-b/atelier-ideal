7e30339e3568b1cbbc5b3a65b0342f2a15f48bf2
gaut-b <gautier.bayle@gmail.com>
2020-05-07 08:41:20 +0200

    still debugging event view on heroku

--- atelier_ideal/website/urls.py
+++ atelier_ideal/website/urls.py
@@ -5,8 +5,8 @@ from .views import ArticleDetailView
 urlpatterns = [
     path('', views.index, name='index'),
     path('events/', views.events, name='events'),
+	path('article/<slug:slug>/', ArticleDetailView.as_view(), name='article-detail'),
 ]
 
 app_name = 'website'
 
-# path('article/<slug:slug>/', ArticleDetailView.as_view(), name='article-detail'),
